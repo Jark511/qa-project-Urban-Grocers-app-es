68290d5a300a7fd1eb099eb6fea37910aaa33132
Jark511 <alfbern16@hotmail.com>
2024-11-14 21:12:20 -0600

    actualizacion

--- configuration.py
+++ configuration.py
@@ -1,3 +1,6 @@
-URL_SERVICE = # Inserta tu dirección de URL sin la barra diagonal al final
-CREATE_USER_PATH = # Almacena la ruta para crear un usuario o usuaria en esta variable
-KITS_PATH = # Almacena la ruta para crear un kit en esta variable
+URL_SERVICE = "https://cnt-74083586-3305-4947-ae86-74701931928e.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users"
+CREATE_KITS_PATH = "/api/v1/kits"
